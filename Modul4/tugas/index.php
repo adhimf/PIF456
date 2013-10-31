887213b68914c81595202af48a6cc34b0a7edce6
Fauzul Adhim <paunjoel@gmail.com>
2013-10-31 14:37:10 +0700

    Modul 4
    Tugas Praktikum

---
+++ Modul4/tugas/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html> 
+ 
+<head> 
+  <title>Akses dan Manipulasi Data</title> 
+  <style type="text/css"> 
+  .even { 
+    background: #ddd; 
+  } 
+  </style>
+	<script language="JavaScript">
+                function konfirmasi(Keterangan){
+                        tanya = confirm('Anda yakin ingin menghapus mahasiswa dengan NIM '+ Keterangan + ' ?');
+                        if (tanya == true) return true;
+                        else return false;
+                }
+        </script>
+  
+</head> 
+ 
+<body> 
+ 
+<?php 
+session_start();
+if(isset($_SESSION['user'])){
+	$user = $_SESSION['user'];
+	echo	'<p align="right">
+			<a href="tugas.php">Logout</a>
+			'.$user.'
+			</p color="red">
+			<div class="border">
+			<p align="center">
+			<br/><br>
+			</font>
+			</p>
+			</div> 
+			
+			';
+	} else {
+		header('location:tugas.php');
+		exit;
+	}
+	
+ini_set('display_errors',1); 
+ 
+// Meng-include file koneksi dan data handler 
+require_once './koneksi.php'; 
+require_once './data_handler.php'; 
+ 
+// Konstanta nama tabel 
+define('MHS', 'mahasiswa'); 
+ 
+// Memanggil fungsi data handler 
+data_handler('?m=data'); 
+ 
+?> 
+ 
+</body> 
+</html>
